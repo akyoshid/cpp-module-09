94256cbac4e60f81b44bd00ff9b0ca448ddc6380
akyoshid <akyoshid@student.42tokyo.jp>
2025-09-12 16:54:11 +0000

    [FIX] continue loading data even if db for ExchangeRate contains invalid data

--- ex00/ExchangeRate.cpp
+++ ex00/ExchangeRate.cpp
@@ -6,12 +6,13 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 14:54:44 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/09/12 14:12:53 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/09/12 16:51:42 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <limits>
+#include <iostream>
 #include "ExchangeRate.hpp"
 
 ExchangeRate::ExchangeRate(const std::string& filename)
@@ -20,10 +21,14 @@ ExchangeRate::ExchangeRate(const std::string& filename)
     std::string line = "";
     while (std::getline(ifs_, line)) {
         ++lineCount_;
-        checkFormat(line);
-        database_.insert(
-            std::make_pair(
-                extractDate(line), extractValue(line.substr(valueIndex_))));
+        try {
+            checkFormat(line);
+            database_.insert(
+                std::make_pair(
+                    extractDate(line), extractValue(line.substr(valueIndex_))));
+        } catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
     if (ifs_.bad())
         throw std::runtime_error(createErrorMessage(0, 0, "could not read"));

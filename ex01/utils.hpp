61c44efbab47dae7b3054593d691fc96392edfb6
akyoshid <akyoshid@student.42tokyo.jp>
2025-09-12 23:49:07 +0000

    [FIX] update header guard

--- ex01/utils.hpp
+++ ex01/utils.hpp
@@ -6,12 +6,12 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 16:41:11 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/09/12 21:12:12 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/09/12 23:46:31 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EX00_UTILS_HPP_
-# define EX00_UTILS_HPP_
+#ifndef EX01_UTILS_HPP_
+# define EX01_UTILS_HPP_
 
 # include <string>
 # include <sstream>
